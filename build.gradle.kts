1f3a20b2fb5515eb80f1b0c51ccb09fa59231815
Joseph Ivie <joseph@lightningkite.com>
2020-09-25 21:22:52 -0600

    Jitpack compatibility attempt

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,4 @@
+import com.lightningkite.khrysalis.gradle.KhrysalisPluginExtension
 group = "org.example"
 version = "1.0-SNAPSHOT"
 
@@ -13,8 +14,26 @@ buildscript {
     dependencies {
         classpath("com.android.tools.build:gradle:4.0.1")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlin_version")
+        classpath("com.lightningkite.khrysalis:plugin:0.1.0")
 
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle files
+    }
+}
+
+subprojects {
+    if(this.name.contains("butterfly")) {
+        val baseName = this.name.removeSuffix("-android")
+
+        apply(plugin = "com.lightningkite.khrysalis")
+
+        configure<KhrysalisPluginExtension> {
+            val p = baseName.split('-').joinToString("") { it.capitalize() }
+            println("Configured name is $p")
+            projectName = p
+            organizationName = "Lightning Kite"
+            overrideIosFolder = project.projectDir.resolve("../../$baseName-ios").toString()
+            overrideWebFolder = project.projectDir.resolve("../../$baseName-web").toString()
+        }
     }
 }
