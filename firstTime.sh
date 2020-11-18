c517af989fc6b975bcc65b3aba8392b8df9c9116
Joseph Ivie <joseph@lightningkite.com>
2020-11-18 16:54:56 -0700

    Build improvements

--- firstTime.sh
+++ firstTime.sh
@@ -1,67 +1,68 @@
+#!/bin/bash
+
 git submodule update
 
-
-cd butterfly-android
+cd butterfly-android || exit
 git checkout master
 cd ..
 
-cd butterfly-bluetooth-android
+cd butterfly-bluetooth-android || exit
 git checkout master
 cd ..
 
-cd butterfly-bluetooth-ios
+cd butterfly-bluetooth-ios || exit
 git checkout master
 cd ..
 
-cd butterfly-fcm-android
+cd butterfly-fcm-android || exit
 git checkout master
 cd ..
 
-cd butterfly-fcm-ios
+cd butterfly-fcm-ios || exit
 git checkout master
 cd ..
 
-cd butterfly-fcm-web
+cd butterfly-fcm-web || exit
 git checkout master
 cd ..
 
-cd butterfly-ios
+cd butterfly-ios || exit
 git checkout master
 cd ..
 
-cd butterfly-maps-android
+cd butterfly-maps-android || exit
 git checkout master
 cd ..
 
-cd butterfly-maps-ios
+cd butterfly-maps-ios || exit
 git checkout master
 cd ..
 
-cd butterfly-maps-ios-google
+cd butterfly-maps-ios-google || exit
 git checkout master
 cd ..
 
-cd butterfly-maps-web-google
+cd butterfly-maps-web-google || exit
 git checkout master
 cd ..
 
-cd butterfly-maps-web-leaflet
+cd butterfly-maps-web-leaflet || exit
 git checkout master
 cd ..
 
-cd butterfly-qr-android
+cd butterfly-qr-android || exit
 git checkout master
 cd ..
 
-cd butterfly-qr-ios
+cd butterfly-qr-ios || exit
 git checkout master
 cd ..
 
-cd butterfly-web
+cd butterfly-web || exit
 git checkout master
 cd ..
 
-cd khrysalis
+cd khrysalis || exit
 git checkout master
 cd ..
 
@@ -70,3 +71,10 @@ cd ..
 ./gradlew --configure-on-demand kotlin-compiler-plugin-swift:publishToMavenLocal
 ./gradlew --configure-on-demand plugin:publishToMavenLocal
 ./gradlew publishToMavenLocal
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+if [ "$KHRYSALIS_META_LOCATION" != "$DIR" ]; then
+  echo "We need to set up an environment variable - KHRYSALIS_META_LOCATION - to this directory.  It will be added to ~/.profile."
+  echo "export KHRYSALIS_META_LOCATION=$DIR" | tee -a ~/.profile
+  source ~/.profile
+fi
