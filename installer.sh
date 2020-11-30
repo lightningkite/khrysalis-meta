e01b50c8b705577938f84a60bafc08f424c2d176
Joseph Ivie <joseph@lightningkite.com>
2020-11-30 12:46:53 -0700

    Updated the installer script

--- installer.sh
+++ installer.sh
@@ -3,15 +3,16 @@
 # This installer is a quick way to get Khrysalis up and running on your computer.
 # It will skip entirely if KHRYSALIS_META_LOCATION is already set.
 
-#if [ -z "$KHRYSALIS_META_LOCATION" ]; then
+if [ -z "$KHRYSALIS_META_LOCATION" ]; then
   echo "Installing Khryalis and Butterfly..."
   git clone https://github.com/lightningkite/khrysalis-meta.git ~/khrysalis-meta
   echo "Clone complete."
   cd ~/khrysalis-meta || exit
-  ~/khrysalis-meta/pullSubmodules.sh
-  ~/khrysalis-meta/firstTime.sh
-#else
-#  cd $KHRYSALIS_META_LOCATION || exit
-#  git checkout master
-#  ./firstTime.sh
-#fi
+  ./pullSubmodules.sh
+  ./firstTime.sh
+else
+  cd $KHRYSALIS_META_LOCATION || exit
+  git checkout master
+  ./pullSubmodules.sh
+  ./firstTime.sh
+fi
