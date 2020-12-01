62896d92ba7e019f090a02fcf2d5c08decf13b7a
Joseph Ivie <josephivie@gmail.com>
2020-12-01 10:01:05 -0700

    BLE rewrite started

--- firstTime.bat
+++ firstTime.bat
@@ -1,5 +1,5 @@
-.\gradlew.bat --configure-on-demand kotlin-compiler-plugin-common:publishToMavenLocal
-.\gradlew.bat --configure-on-demand kotlin-compiler-plugin-typescript:publishToMavenLocal
-.\gradlew.bat --configure-on-demand kotlin-compiler-plugin-swift:publishToMavenLocal
-.\gradlew.bat --configure-on-demand plugin:publishToMavenLocal
-.\gradlew.bat publishToMavenLocal
+call .\gradlew.bat --configure-on-demand kotlin-compiler-plugin-common:publishToMavenLocal
+call .\gradlew.bat --configure-on-demand kotlin-compiler-plugin-typescript:publishToMavenLocal
+call .\gradlew.bat --configure-on-demand kotlin-compiler-plugin-swift:publishToMavenLocal
+call .\gradlew.bat --configure-on-demand plugin:publishToMavenLocal
+call .\gradlew.bat publishToMavenLocal
